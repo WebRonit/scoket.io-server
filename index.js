b6babd2da53d2ff32d6acd25752a55a9c5657acc
Ronit Dutta <96005252+WebRonit@users.noreply.github.com>
2024-03-05 10:07:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
-const io = require('socket.io')(process.env.PORT, {
+const io = require('socket.io')(process.env.PORT || 3000, {
     cors: {
-        origin: "https://chatsroom.vercel.app",
-        methods: ["GET", "POST"]
+        origin: "*"
+        // methods: ["GET", "POST"]
     }
-}); //https://webchats1.netlify.app
+}); 
 
 const users = {}
+const MAX_USER = 2;
+let connectedUsers = 0;
 
 io.on('connection', socket => {
 
-    socket.on('newUserJoined', name => {
-        console.log("User joined: ", name)
-        users[socket.id] = name;
-        socket.broadcast.emit("userJoined", name);
+    connectedUsers++;
+
+    if(connectedUsers <= MAX_USER){
+
+         socket.on('newUserJoined', name => {
+         console.log("User joined: ", name);
+         console.log("number of users:", connectedUsers);
+         users[socket.id] = name;
+         socket.broadcast.emit("userJoined", name);
+      });
+
+      socket.on('disconnect', () =>{
+         connectedUsers--;
+         console.log(`User disconnected. Total users: ${connectedUsers}`);
+      })
+    }
+
+    else{
+        console.log("Max user limit reached!")
+        socket.emit('maxUserReached', { message: 'Maximum users limit reached' });
+        socket.disconnect(true);
+    }
+   
+    socket.on('typing', name =>{
+        socket.broadcast.emit('userTyping', {name: users[socket.id]});
     });
+
+     socket.on('notTyping', () => {
+        socket.broadcast.emit('userTypingFalse');
+    })
 
     socket.on('newMsg', message => {
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
-    })
+    });
 
 });
